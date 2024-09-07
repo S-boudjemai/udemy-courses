b46c935b3cd8eec928cbfeba9c8bc3a7c1d86e62
Sofiane <sofiane.boudjemai.pro@gmail.com>
2024-09-07 18:10:47 +0200

    first commit

---
+++ src/create-event.dto.ts
@@ -0,0 +1,7 @@
+export class CreateEventDto {
+  // Dans ce fichier, nous avons créé une classe DTO afin de définir la structure des données que nous attendons pour créer un événement.
+  name: string;
+  description: string;
+  when: string;
+  address: string;
+}
